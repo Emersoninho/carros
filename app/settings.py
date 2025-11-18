17b43c602e2c639b1352bd56393754cb667ed62d
Emerson <emersoneletrotecnico2013@gmail.com>
2025-11-18 14:03:28 -0300

    Switch to SQLite for Render free

--- app/settings.py
+++ app/settings.py
@@ -79,27 +79,35 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
-if DATABASE_URL:
-    # Render
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True
-        )
+# if DATABASE_URL:
+#     # Render
+#     DATABASES = {
+#         'default': dj_database_url.config(
+#             default=DATABASE_URL,
+#             conn_max_age=600,
+#             ssl_require=True
+#         )
+#     }
+# else:
+#     # LOCALHOST
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'carros',
+#             'USER': 'postgres',
+#             'PASSWORD': 'Helena@06072018',
+#             'HOST': 'localhost',
+#             'PORT': '5432',
+#         }
+#     }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    # LOCALHOST
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'carros',
-            'USER': 'postgres',
-            'PASSWORD': 'Helena@06072018',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
-    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
